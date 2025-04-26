e5a3cc833217373e993d7eac326aa2f8cd7c80bf
Rabia Yavuluel <153555184+103105116107@users.noreply.github.com>
2025-04-26 21:26:17 +0300

    Initial Commit

---
+++ app/src/androidTest/java/com/example/odev1/odev_1.kt
@@ -0,0 +1,50 @@
+package com.example.odev1
+
+fun useAppContext() {
+    val sehir: String = "Ankara"
+    val ulke: String = "Türkiye"
+    val telefon = "05555555555"
+    val postakodu = "06000"
+    val email= "email@ornek.com"
+    val meslek: String = "Bilgisayar Mühendisi"
+    var stokMiktari: Int = 3400
+    val musteriAdi: String = "Atilla"
+    var bakiye: Int = 9876
+    val dogumGunu ="10.07.2000"
+    var maas: Int= 33000
+    var medeniDurum: String = "Bekar"
+    val urunYorum: String = "Harika bir ürün!"
+    val odemeTarihi = "01.01.2025"
+    val odeme: Int= 550
+    val siparisAdedi: Int = 240
+    val arabaModeli: String = "Toyota- Supra"
+    val kitapAdi: String = "Martin Eden"
+    val yayinTarihi = "09.09.1909"
+    var indirimMiktari: Int = 400
+    val odaSayisi: Int = 4
+    val enlem: Int = 125
+    val boylam:Int = 120
+    val urunAdi: String = "Kitaplık"
+    val yemekFiyati: Int = 350
+    val marka: String = "Ithaki"
+    val muzikAdi: String = "Heavy is the crown"
+    val videoSuresi: Double = 3.12
+    val urunPuani = 4.9
+    val resimAdi = "img.jpg"
+    val dosyaFormati = "txt"
+    val renk = "Mavi"
+    val renkKodu = "#B6E4FB"
+    val telefonModeli = "Samsung s21 ultra lite"
+    val ekranBoyutu = 6.8
+    val agirlik = 227
+    val ulusalGun = "30 Ağustos Zafer Bayramı"
+    val tatilGunu = "Ramazan Bayramı"
+    val rezervasyonTarihi = "11.11.2025"
+    val sokakAdi = "Cumhuriyet Caddesi"
+    val otobusHatti = "K7"
+    val kalanDakika = 3
+    val takipKodu = "AB12CD34"
+    val kuponSuresi = "12 saat"
+    val kuponKodu = "NEMUTLU2910"
+    val faturaAdresi = "Kızılay, Ankara"
+}
